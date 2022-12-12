863b12e953b852fddddcc9471439494118463a61
topscoder <86197446+topscoder@users.noreply.github.com>
2022-12-12 15:44:36 +0100

    Solved day 6 part 2

---
+++ 6/solve.part2.py
@@ -0,0 +1,24 @@
+
+# bvwbjplbgvbhsrlpgdmjqwftvncz: first marker after character 5
+
+
+def start_of_packet(buffer):
+    i = 0
+    while i < len(buffer) - 14:
+        start = 0 + i
+        length = 14 + i
+        chunk = buffer[start:length]
+
+        # Remove duplicate characters from string
+        if len(chunk) == len(''.join(dict.fromkeys(chunk))):
+            # start of packet found
+            return length
+
+        i = i + 1
+
+
+with open('input.txt') as f:
+    lines = f.read().split("\n")
+    for line in lines:
+        if len(line.strip()) > 0:
+            print(f"First marker after character: {start_of_packet(line)}")
